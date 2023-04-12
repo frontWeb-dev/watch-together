b20d7f6acd2064d92bf3174b7b099d6e3c42c3a0
frontweb-dev <frontend_web@kakao.com>
2023-04-12 19:37:36 +0900

    issue : 페이지네이션 이슈 수정
    5 , 10 페이지를 누를 경우, 6으로 바로 넘어가버리는 오류 수정

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,15 +1,15 @@
-"use client";
-import Header from "@/components/Header";
-import SignInForm from "@/components/SignInForm";
+'use client';
+import Header from '@/components/Header';
+import SignInForm from '@/components/SignInForm';
 
 export default function Page() {
   return (
     <div>
-      <Header title="로그인" goBack="/" />
+      <Header title='로그인' goBack='/' />
 
-      <div className="flex h-screen w-full flex-col justify-center space-y-10 overflow-hidden p-4">
+      <div className='flex h-screen w-full flex-col justify-center space-y-10 overflow-hidden p-4'>
         <div>
-          <p className="mb-4 text-2xl font-bold">
+          <p className='mb-4 text-2xl font-bold'>
             쉬운 파티원 초대와 매칭, <br />
             결제까지 한번에
             <br />
@@ -17,7 +17,7 @@ export default function Page() {
           <p>
             이 모든 것을 가능하게 하는
             <br />
-            같이 보는 가치, <span className="font-bold">가치 와치!</span>
+            같이 보는 가치, <span className='font-bold'>가치 와치!</span>
           </p>
         </div>
         <SignInForm />
--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -1,7 +1,7 @@
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import Input from "@/components/Input";
-import Button from "@/components/Button";
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
 
 interface SignInForm {
   email: string;
@@ -14,42 +14,40 @@ export default function SignInForm() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<SignInForm>({ mode: "onChange" });
+  } = useForm<SignInForm>({ mode: 'onChange' });
 
   const onValid = (formData: SignInForm) => {
     console.log(formData);
   };
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)} className="mb-8 space-y-2">
+      <form onSubmit={handleSubmit(onValid)} className='mb-8 space-y-2'>
         <Input
-          register={register("email", {
-            required: "이메일을 입력하세요",
+          register={register('email', {
+            required: '이메일을 입력하세요',
           })}
-          type="email"
-          name="email"
-          placeholder="Email"
-        >
+          type='email'
+          name='email'
+          placeholder='Email'>
           {errors.email?.message}
         </Input>
 
         <Input
-          register={register("password", {
-            required: "비밀번호를 입력하세요",
+          register={register('password', {
+            required: '비밀번호를 입력하세요',
           })}
-          type="password"
-          name="password"
-          placeholder="Password"
-        >
+          type='password'
+          name='password'
+          placeholder='Password'>
           {errors.password?.message}
         </Input>
-        <Button type="submit" text="로그인" large common />
+        <Button type='submit' text='로그인' large common />
       </form>
-      <div className="relative border-t pt-8">
-        <p className="absolute -top-3 left-1/2 -translate-x-1/2 bg-white px-3 text-gray-500">
+      <div className='relative border-t pt-8'>
+        <p className='absolute -top-3 left-1/2 -translate-x-1/2 bg-white px-3 text-gray-500'>
           또는
         </p>
-        <Button onclick={() => router.push("/sign-up")} text="계정 만들기" large />
+        <Button onclick={() => router.push('/sign-up')} text='계정 만들기' large />
       </div>
     </div>
   );
