1f4fa4118f4eefae91fc7221dfe138d8166760c4
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2023-04-25 21:12:46 +0900

    feat: axios, react-toasitify

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
-import './globals.css';
+import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
-  title: '가치와치',
-  description: 'OTT 계정 공유 서비스',
+  title: "가치와치",
+  description: "OTT 계정 공유 서비스",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='ko'>
+    <html lang="ko">
       <body>
-        <div className='mx-auto w-full max-w-md bg-white'>
+        <div className="mx-auto w-full max-w-md bg-white">
           <div>{children}</div>
         </div>
       </body>
